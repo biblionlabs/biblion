e2a7c157b07a3b121e34343d7649d543e4850c87
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-13 19:53:23 -0400

    chore: init proyect

---
+++ justfile
@@ -0,0 +1,19 @@
+genkey key_alias key_store:
+    # Run keytool to generate key
+    docker run --rm -it -v "$(pwd)/:/src" -w /src --entrypoint keytool rust-android:1.90-sdk-36 \
+        -genkey -v -keystore {{key_store}} -alias {{key_alias}} -keyalg RSA -keysize 2048 -validity 10000
+
+build:
+    docker run --rm -it -v "$(pwd)/:/src" -v "$(pwd)/.gradle/caches:/root/.gradle/caches" -w /src rust-android:1.90-sdk-36 assembleRelease
+
+sign key_alias key_store:
+    # Run apksigner to sign generated apk
+    docker run --rm -it -v "$(pwd)/:/src" -w /src --entrypoint apksigner rust-android:1.90-sdk-36 \
+        sign --ks-key-alias {{key_alias}} --ks {{key_store}} android/build/outputs/apk/release/android-release-unsigned.apk
+    sudo cp android/build/outputs/apk/release/android-release-unsigned.apk \
+        android/build/outputs/apk/release/android-release-signed.apk
+
+install:
+    adb install android/build/outputs/apk/release/android-release-signed.apk
+
+run key_alias key_store: (build) (sign key_alias key_store) (install)
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use freya::prelude::*;
+
+#[cfg(target_os = "android")]
+use winit::platform::android::activity::AndroidApp;
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(droid_app: AndroidApp) {
+    use winit::platform::android::EventLoopBuilderExtAndroid;
+
+    launch(
+        LaunchConfig::new().with_window(
+            WindowConfig::new(app)
+                .with_size(500., 450.)
+                .with_window_attributes(|_attr, event_loop_builder| {
+                    event_loop_builder.with_android_app(droid_app)
+                }),
+        ),
+    )
+}
+
+#[allow(dead_code)]
+#[cfg(not(target_os = "android"))]
+fn main() {
+    launch(LaunchConfig::new().with_window(WindowConfig::new(app).with_size(600., 450.)))
+}
+
+fn app() -> impl IntoElement {
+    let mut count = use_state(|| 4);
+
+    let counter = rect()
+        .width(Size::fill())
+        .height(Size::percent(50.))
+        .center()
+        .color((255, 255, 255))
+        .background((15, 163, 242))
+        .font_weight(FontWeight::BOLD)
+        .font_size(75.)
+        .shadow((0., 4., 20., 4., (0, 0, 0, 80)))
+        .child(count.read().to_string());
+
+    let actions = rect()
+        .horizontal()
+        .width(Size::fill())
+        .height(Size::percent(50.))
+        .center()
+        .spacing(8.0)
+        .child(
+            Button::new()
+                .on_press(move |_| {
+                    *count.write() += 1;
+                })
+                .child("Increase"),
+        )
+        .child(
+            Button::new()
+                .on_press(move |_| {
+                    *count.write() -= 1;
+                })
+                .child("Decrease"),
+        );
+
+    rect().child(counter).child(actions)
+}
