b8658432cff0f90db86f921461c76a6316e946dc
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-19 19:39:22 -0400

    feat: add tracing logs to file

--- src/lib.rs
+++ src/lib.rs
@@ -33,5 +33,27 @@ fn android_main(droid_app: AndroidApp) {
 #[allow(dead_code)]
 #[cfg(not(target_os = "android"))]
 fn main() {
+    use tracing::level_filters::LevelFilter;
+    use tracing_subscriber::EnvFilter;
+
+    use self::utils::data_dir;
+
+    let builder = tracing_appender::rolling::Builder::new()
+        .rotation(tracing_appender::rolling::Rotation::DAILY)
+        .filename_suffix("log")
+        .build(data_dir(&["logs"]))
+        .unwrap();
+    let (non_blocking, _guard) = tracing_appender::non_blocking(builder);
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .try_from_env()
+                .unwrap_or_default(),
+        )
+        .with_writer(non_blocking)
+        .init();
+
     launch(LaunchConfig::new().with_window(WindowConfig::new(init).with_size(600., 450.)))
 }
