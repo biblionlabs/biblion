c8ac49a2abf139d58dab1cca667e0c08656b453a
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-26 15:29:35 -0400

    feat: move lib into main to have more control of binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-use freya::prelude::*;
-
-mod app;
-mod components;
-mod dialog;
-mod utils;
-
-use app::init;
+pub mod app;
+pub mod components;
+pub mod dialog;
+pub mod utils;
 
 pub const APP_NAME: &str = env!("CARGO_CRATE_NAME");
 
 #[cfg(target_os = "android")]
-use winit::platform::android::activity::AndroidApp;
+use {app::init, freya::prelude::*, winit::platform::android::activity::AndroidApp};
 
 #[cfg(target_os = "android")]
 #[no_mangle]
@@ -30,32 +22,4 @@ fn android_main(droid_app: AndroidApp) {
                 }),
         ),
     )
-}
-
-#[allow(dead_code)]
-#[cfg(not(target_os = "android"))]
-fn main() {
-    use tracing::level_filters::LevelFilter;
-    use tracing_subscriber::EnvFilter;
-
-    use self::utils::data_dir;
-
-    let builder = tracing_appender::rolling::Builder::new()
-        .rotation(tracing_appender::rolling::Rotation::DAILY)
-        .filename_suffix("log")
-        .build(data_dir(&["logs"]))
-        .unwrap();
-    let (non_blocking, _guard) = tracing_appender::non_blocking(builder);
-    tracing_subscriber::fmt()
-        .with_ansi(false)
-        .with_env_filter(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::TRACE.into())
-                .try_from_env()
-                .unwrap_or_default(),
-        )
-        .with_writer(non_blocking)
-        .init();
-
-    launch(LaunchConfig::new().with_window(WindowConfig::new(init).with_size(600., 450.)))
 }
